05c62089d249620015e799cd7da2618945fcdc79
Damien Sawyer <damiensawyer@gmail.com>
2021-11-02 00:44:03 +0100

    bringing in moralis address component

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,8 +1,10 @@
+import Address from "../../components/Address/Address";
 
 const Home = () => (
     <div className="App">
         <header className="App-header">
             <h1>about</h1>
+            <Address />
         </header>
     </div>
 );
