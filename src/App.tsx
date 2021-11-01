9b5e924ab716a762ddf41144df2b7d8ccfbc03b7
Damien Sawyer <damiensawyer@gmail.com>
2021-11-01 01:50:47 +0100

    ping pong

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './content/substratmlogo.png';
 import './App.css';
+import {useAppSelector, useAppDispatch} from "./features/hooks";
 import { add } from './functions/maths';
+import {setPing, setPong} from "./features/counter.slice";
 
 function App() {
+  let pingValue = useAppSelector(x => x.counterSlice.pingValue)
+   let dispatch = useAppDispatch()
+   
+    useEffect(() => {
+        dispatch(setPing())
+    }, []);
+    
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        
-        </p>
         <a className="App-link" target="_blank" href="https://chain.link/hackathon"><pre>https://chain.link/hackathon</pre></a>      
-        <pre>dev</pre>    
+        <pre>dev {pingValue}</pre>    
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {Provider} from "react-redux";
 import App from './App';
+import {useAppDispatch} from "./features/hooks";
 import reportWebVitals from './reportWebVitals';
 import {store} from './features/store'
 
