56713f0cbe831e52f2cc18e594e47bec49240040
Damien Sawyer <damiensawyer@gmail.com>
2021-11-07 03:48:31 +0000

    metamask

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,16 +1,21 @@
 import {Button, CssBaseline} from "@mui/material";
 import {useAppDispatch, useAppSelector} from "../../slices/hooks";
 import {toggleLogin} from "../../slices/login.slice";
-
+import { useMoralis } from "react-moralis";
 const Login = () => {
     let dispatch = useAppDispatch();
     let isLoggedIn = useAppSelector(x => x.loginSlice.loggedIn)
+    const { authenticate, isAuthenticated, logout } = useMoralis();
     return <div className="App">
         
         <header className="App-header">
             <h1>Login</h1>
             <h2>Logged In: {isLoggedIn ? 'yes' : 'no'}</h2>
             <Button variant="contained" onClick={() => dispatch(toggleLogin())}>Toggle Login</Button>
+
+
+            <h2>Moralis</h2>
+            <Button variant="contained" onClick={() => authenticate({ signingMessage: "Hello World!" })}>Login with Metamask</Button>
         </header>
     </div>
     
