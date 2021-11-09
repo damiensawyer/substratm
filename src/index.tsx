46406b2b7683eafb2097318c4ec4e09b424423d8
Damien Sawyer <damiensawyer@gmail.com>
2021-11-09 04:24:07 +0000

    env

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,12 @@ import { theme } from './content/helpers';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { MoralisProvider } from 'react-moralis';
 
-const APP_ID = 'p9s6FPKleg69I1myRSx8pTDboCsouzoXCj4M5Osw'; // move to env // process.env.REACT_APP_MORALIS_APPLICATION_ID;
-const SERVER_URL = 'https://42sf9cpyfott.usemoralis.com:2053/server'; //  process.env.REACT_APP_MORALIS_SERVER_URL;
+// I think that these are set in .env in the root folder. The .env file is in .gitignore, we each need to set our own.
+const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
+const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
+
+// const APP_ID = 'p9s6FPKleg69I1myRSx8pTDboCsouzoXCj4M5Osw'; // move to env // process.env.REACT_APP_MORALIS_APPLICATION_ID;
+// const SERVER_URL = 'https://42sf9cpyfott.usemoralis.com:2053/server'; //  process.env.REACT_APP_MORALIS_SERVER_URL;
 
 ReactDOM.render(
   <React.StrictMode>
