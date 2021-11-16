7a1b9a3c25865729b9d9384a4067009e2e6d1a4d
lil14n 8u <80593539+0xLil14n@users.noreply.github.com>
2021-11-15 21:40:38 -0600

    rm TwitterVerification.sol + update README with verification details (#16)

--- contracts/TwitterVerification.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-contract TwitterVerification {
-    address public owner;
-    constructor() public {
-        owner = msg.sender;
-    }
-}
