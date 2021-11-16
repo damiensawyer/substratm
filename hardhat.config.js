e245bbe626614c2aaddeac3a9f94f0792978add7
0xLil14n <0xLil14n@gmail.com>
2021-11-15 21:40:00 -0500

    adding hardhat deploy + updating readme for deploying to rinkeby/ropsten

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,39 +1,31 @@
+
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-// const ROPSTEN_PRIVATE_KEY = "YOUR ROPSTEN PRIVATE KEY";
+const privateKey = process.env.PRIVATE_KEY
+const accounts = {accounts: [`0x${privateKey}`]}
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async () => {});
 
 module.exports = {
-  solidity: "0.8.0",
-  networks: {
-//    ropsten: {
-//      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-//      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
-//    },
-    rinkeby: {
-      provider: function() {
-        return new HDWalletProvider(
-          privateKeys,
-          RINKEBY_RPC_URL
-        )
-      },
-      gas: 10000000,
-      gasPrice: 5000000000, // 5 gwei
-      network_id: 4
+    solidity: "0.8.0",
+    networks: {
+        ropsten: {
+          url: `https://speedy-nodes-nyc.moralis.io/72216de496ff399faf1f925a/eth/ropsten`,
+          ...accounts
+        },
+        rinkeby: {
+            url: "https://speedy-nodes-nyc.moralis.io/72216de496ff399faf1f925a/eth/rinkeby",
+            ...accounts
+        }
     },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(
-          privateKeys,
-          ROPSTEN_RPC_URL
-        )
-      },
-      gas: 50000000,
-      gasPrice: 5000000000, // 5 gwei
-      network_id: 3
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY
     }
-  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,16 @@
 async function main() {
     const SubstratmNFT = await ethers.getContractFactory("SubstratmNFT");
     const deployedSubstratmContract = await SubstratmNFT.deploy();
-  
+
     console.log("SubstratmNFT Address deployed to:", deployedSubstratmContract.address);
-  }
-  
-  main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
+    return deployedSubstratmContract.address;
+}
+
+main()
+.then(() => process.exit(0))
+.catch(
+    (error) => {
+        console.error(error);
+        process.exit(1);
+    }
+);
