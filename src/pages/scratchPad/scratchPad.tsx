acf068d8901cdfa72afaf07d385156c2575e226e
0xLil14n <0xLil14n@gmail.com>
2022-01-10 10:18:42 -0500

    adding contractAddress update to FE on deploy

--- src/pages/scratchPad/scratchPad.tsx
+++ src/pages/scratchPad/scratchPad.tsx
@@ -1,185 +1,228 @@
-import {Label} from "@material-ui/icons";
+import { Label } from '@material-ui/icons';
 import ReactJson from 'react-json-view';
-import {Button, TextField} from "@mui/material";
-import Moralis from "moralis";
-import React, {useEffect, useState} from "react";
-import {useMoralis, useMoralisWeb3Api, useMoralisWeb3ApiCall} from "react-moralis";
+import { Button, TextField } from '@mui/material';
+import Moralis from 'moralis';
+import React, { useEffect, useState } from 'react';
+import {
+  useMoralis,
+  useMoralisWeb3Api,
+  useMoralisWeb3ApiCall,
+} from 'react-moralis';
 import { Card } from '@mui/material';
-
+import { contractAddress } from '../../utils/contractAddress';
 import Address from '../../components/Address/Address';
-import {MyPaper, SmallPre, StyledPaper} from "../../content/commonStyles";
-import {RaisedPaper, RaisedPaperCode} from "../../content/componentStyles";
+import { MyPaper, SmallPre, StyledPaper } from '../../content/commonStyles';
+import { RaisedPaper, RaisedPaperCode } from '../../content/componentStyles';
 // import {SubstratmNFTABI} from "../../functions/SubstratmNFTABI";
 import SubstratmNFT from '../../artifacts/contracts/SubstratmNFT.sol/SubstratmNFT.json';
 
-declare const ethers: any
+declare const ethers: any;
 const About = () => {
-    const {
-        isWeb3Enabled,
-        isAuthenticated,
-        enableWeb3,
-        authenticate,
-        logout,
-        isAuthenticating,
-        isWeb3EnableLoading,
-        user
-    } = useMoralis();
+  const {
+    isWeb3Enabled,
+    isAuthenticated,
+    enableWeb3,
+    authenticate,
+    logout,
+    isAuthenticating,
+    isWeb3EnableLoading,
+    user,
+  } = useMoralis();
 
-    var u: string = JSON.stringify(user, undefined, 2)
-    const damien = (process.env.REACT_APP_DAMIEN as string);
-    const {account: {getNativeBalance, getTokenBalances}} = useMoralisWeb3Api()
-    const Web3Api = useMoralisWeb3Api()
-    const [sampleBlock, setSampleBlock] = useState('loading sample block....');
-    const [nftResult, setNftResult] = useState(null);
-    const [twitterHandle, setTwitterHandle] = useState({});
-    const [tweetId, setTweetId] = useState({});
-    const [nftMintedForAddress, setNftMintedForAddress] = useState<boolean | null>(null); // should re
-    const [twitterHandleRetrievedFromContract, setTwitterHandleRetrievedFromContract] = useState<string | null>(null);
+  var u: string = JSON.stringify(user, undefined, 2);
+  const damien = process.env.REACT_APP_DAMIEN as string;
+  const {
+    account: { getNativeBalance, getTokenBalances },
+  } = useMoralisWeb3Api();
+  const Web3Api = useMoralisWeb3Api();
+  const [sampleBlock, setSampleBlock] = useState('loading sample block....');
+  const [nftResult, setNftResult] = useState(null);
+  const [twitterHandle, setTwitterHandle] = useState({});
+  const [tweetId, setTweetId] = useState({});
+  const [nftMintedForAddress, setNftMintedForAddress] = useState<
+    boolean | null
+  >(null); // should re
+  const [
+    twitterHandleRetrievedFromContract,
+    setTwitterHandleRetrievedFromContract,
+  ] = useState<string | null>(null);
 
+  const getUserAddress = async () => {
+    let web3 = await Moralis.Web3.enableWeb3();
+    let accounts = await web3.eth.getAccounts();
+    return accounts[0] as string;
+  };
 
-    const CONTRACT_ADDRESS = "0x7bFDFd600620BAeA044c2e95F0C562B5FaC0454f" // deployed on ropsten
-// TODO make this not hardcoded lmao
-    const getUserAddress = async () => {
-        let web3 = await Moralis.Web3.enableWeb3()
-        let accounts = await web3.eth.getAccounts()
-        return accounts[0] as string;
+  // Uses https://github.com/MoralisWeb3/react-moralis#resolve-data-with-usemoralisweb3apicall
+  // This is not ideal as it will re-execute the call alot. Use useMoralisWeb3ApiCall instead passing in dependencies.
+  const fetchBlock = async () => {
+    const result = await Web3Api.native.getBlock({
+      block_number_or_hash: '4614776',
+    });
+    result.transactions = [result.transactions[0]]; // Trim the transactions
+    setSampleBlock(JSON.stringify(result, null, 2));
+  };
+
+  useEffect(() => {
+    fetchBlock();
+  });
+
+  // https://youtu.be/rd0TTLjQLy4?t=1152
+  const optionsCore = {
+    contractAddress,
+    abi: SubstratmNFT.abi,
+  };
+
+  const checkIfNFTMintedForUser = async () => {
+    let userAddress = await getUserAddress();
+
+    let options = {
+      functionName: 'nftExistsForAccount',
+      params: {
+        userAddress: userAddress,
+      },
+      ...optionsCore,
     };
 
-    // Uses https://github.com/MoralisWeb3/react-moralis#resolve-data-with-usemoralisweb3apicall
-    // This is not ideal as it will re-execute the call alot. Use useMoralisWeb3ApiCall instead passing in dependencies. 
-    const fetchBlock = async () => {
-        const result = await Web3Api.native.getBlock({
-            block_number_or_hash: '4614776'
-        })
-        result.transactions = [result.transactions[0]]; // Trim the transactions
-        setSampleBlock(JSON.stringify(result, null, 2))
-    }
+    let result = await Moralis.Web3.executeFunction(options);
+    setNftMintedForAddress(result as boolean);
+    console.log('check address minted', result);
+  };
 
-    useEffect(() => {
-        fetchBlock()
-    })
+  const loadTwitterHandle = async () => {
+    let userAddress = await getUserAddress();
 
-
-    // https://youtu.be/rd0TTLjQLy4?t=1152
-    const optionsCore = {
-        contractAddress: CONTRACT_ADDRESS,
-        abi: SubstratmNFT.abi,
-
+    let options = {
+      functionName: 'readTwitterHandleForGivenAddress',
+      params: {
+        userAddress: userAddress,
+      },
+      ...optionsCore,
     };
 
-    const checkIfNFTMintedForUser = async () => {
-        let userAddress = await getUserAddress()
+    let result = await Moralis.Web3.executeFunction(options);
+    setTwitterHandleRetrievedFromContract(result as string);
+    console.log('retrieved Twitter Handle', result);
+  };
 
-        let options = {
-            functionName: "nftExistsForAccount",
-            params: {
-                userAddress: userAddress
-            }, ...optionsCore
-        }
-
-        let result = await Moralis.Web3.executeFunction(options)
-        setNftMintedForAddress(result as boolean)
-        console.log('check address minted', result)
-
-    }    
-    
-        const loadTwitterHandle = async () => {
-        let userAddress = await getUserAddress()
-
-        let options = {
-            functionName: "readTwitterHandleForGivenAddress",
-            params: {
-                userAddress: userAddress
-            }, ...optionsCore
-        }
-
-        let result = await Moralis.Web3.executeFunction(options)
-        setTwitterHandleRetrievedFromContract(result as string)
-        console.log('retrieved Twitter Handle', result)
-
+  const mintNFT = async () => {
+    const options = {
+      functionName: 'requestToMintNewSubstratmProfileNFT',
+      ...optionsCore,
+    };
+    try {
+      const result = await Moralis.Web3.executeFunction(options);
+      setNftResult(result);
+      console.log('result', result);
+    } catch (e) {
+      console.log('error', e);
     }
+  };
+  const validateTwitterData = async () => {
+    console.log('validating twitter data...');
+    const userAddress = await getUserAddress();
 
-
-    const mintNFT = async () => {
-
-        const options = {
-            functionName: "requestToMintNewSubstratmProfileNFT",
-            ...optionsCore
-        }
-        try {
-            const result = await Moralis.Web3.executeFunction(options)
-            setNftResult(result)
-            console.log('result', result)
-        } catch (e) {
-            console.log('error', e);
-        }
+    console.log('userAddress', userAddress);
+    const options = {
+      functionName: 'updateSubstratmProfile',
+      params: {
+        twitterHandle: twitterHandle,
+        tweetId: tweetId,
+        verificationString: 'verification',
+      },
+      userAddress: userAddress,
+      ...optionsCore,
+    };
+    try {
+      const result = await Moralis.Web3.executeFunction(options);
+      console.log('result', result);
+    } catch (e) {
+      console.log('error', e);
     }
-    const validateTwitterData = async () => {
-        console.log('validating twitter data...');
-        const userAddress = await getUserAddress()
+  };
 
-        console.log('userAddress', userAddress);
-        const options = {
-            functionName: "updateSubstratmProfile",
-            params: {
-                'twitterHandle': twitterHandle,
-                'tweetId': tweetId,
-                'verificationString': 'verification'
-            },
-            userAddress: userAddress,
-            ...optionsCore
-        }
-        try {
-            const result = await Moralis.Web3.executeFunction(options);
-            console.log('result', result);
-        } catch (e) {
-            console.log('error', e);
-        }
-    }
+  // note, these fail if you hit f5, even though we're connected to meta
+  const {
+    data: d1,
+    isFetching: f1,
+    error: e1,
+  } = useMoralisWeb3ApiCall(getNativeBalance); // https://github.com/MoralisWeb3/react-moralis#resolve-data-with-usemoralisweb3apicall mask.
+  const {
+    data: d2,
+    isFetching: f2,
+    error: e2,
+  } = useMoralisWeb3ApiCall(getTokenBalances);
 
-    // note, these fail if you hit f5, even though we're connected to meta
-    const {data: d1, isFetching: f1, error: e1} = useMoralisWeb3ApiCall(getNativeBalance) // https://github.com/MoralisWeb3/react-moralis#resolve-data-with-usemoralisweb3apicall mask. 
-    const {data: d2, isFetching: f2, error: e2} = useMoralisWeb3ApiCall(getTokenBalances)
+  const moralisData = {
+    User: Moralis.User.current(),
+    serverURL: Moralis.serverURL,
+  };
+  const twitterVerification = (
+    <RaisedPaperCode>
+      <div>
+        <h3>
+          Connect your Twitter Handle to your account by tweeting
+          "Verification":
+        </h3>
+        <TextField
+          id="outlined-basic"
+          label="Twitter Handle"
+          variant="outlined"
+          onChange={(event) => setTwitterHandle(event.target.value)}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Tweet Id"
+          variant="outlined"
+          onChange={(event) => setTweetId(event.target.value)}
+        />
+        <br />
+        <Button variant="contained" onClick={validateTwitterData}>
+          Connect Twitter Handle To Profile
+        </Button>
+      </div>
+    </RaisedPaperCode>
+  );
+  return (
+    <>
+      <h1>Mint Substratm NFT</h1>
+      <div>
+        <Button variant="contained" onClick={mintNFT}>
+          Mint NFT
+        </Button>
+        {/*{!!nftResult && <><h5>Minted an NFT...</h5><RaisedPaperCode><ReactJson src={nftResult} theme="monokai"/></RaisedPaperCode></>}*/}
+      </div>
+      <br />
+      {twitterVerification}
 
+      <h3>Checking NFT Minted Status for current metamask account</h3>
+      <div>
+        <Button variant="contained" onClick={checkIfNFTMintedForUser}>
+          Check if Address Minted (note, on Ropsten, needed to wait a few
+          seconds for miners. )
+        </Button>
+        {nftMintedForAddress !== null && (
+          <>
+            <h5>NFT Minted: </h5>
+            {nftMintedForAddress ? 'yes, minted' : 'no, not minted'}{' '}
+          </>
+        )}
+      </div>
 
-    const moralisData = {
-        User: Moralis.User.current(),
-        serverURL: Moralis.serverURL
-    }
-    const twitterVerification = (
-        <RaisedPaperCode>
-            <div>
-                <h3>Connect your Twitter Handle to your account by tweeting "Verification":</h3>
-                <TextField id="outlined-basic" label="Twitter Handle" variant="outlined" onChange={event => setTwitterHandle(event.target.value)}/>
-                <TextField id="outlined-basic" label="Tweet Id" variant="outlined" onChange={event => setTweetId(event.target.value)}/>
-                <br/>
-                <Button variant="contained" onClick={validateTwitterData}>Connect Twitter Handle To Profile</Button>
-            </div>
-        </RaisedPaperCode>
-    );
-    return <>
-        <h1>Mint Substratm NFT</h1>
-        <div>
-            <Button variant="contained" onClick={mintNFT}>Mint NFT</Button>
-            {/*{!!nftResult && <><h5>Minted an NFT...</h5><RaisedPaperCode><ReactJson src={nftResult} theme="monokai"/></RaisedPaperCode></>}*/}
-        </div>
-            <br/>
-        {twitterVerification}
-
-        <h3>Checking NFT Minted Status for current metamask account</h3>
-        <div>
-
-            <Button variant="contained" onClick={checkIfNFTMintedForUser}>Check if Address Minted (note, on Ropsten, needed to wait a few seconds for miners. )</Button>
-            {nftMintedForAddress !== null && <><h5>NFT Minted: </h5>{nftMintedForAddress ? 'yes, minted' : 'no, not minted'} </>}
-        </div>
-
-        <h3>Read Twitter handle from contract for current metamask account</h3>
-        <div>
-
-            <Button variant="contained" onClick={loadTwitterHandle}>Read Twitter Handle from Contract</Button>
-            {twitterHandleRetrievedFromContract !== null && <><h5>Twitter Handle: </h5>{twitterHandleRetrievedFromContract} </>}
-        </div>
+      <h3>Read Twitter handle from contract for current metamask account</h3>
+      <div>
+        <Button variant="contained" onClick={loadTwitterHandle}>
+          Read Twitter Handle from Contract
+        </Button>
+        {twitterHandleRetrievedFromContract !== null && (
+          <>
+            <h5>Twitter Handle: </h5>
+            {twitterHandleRetrievedFromContract}{' '}
+          </>
+        )}
+      </div>
     </>
+  );
 };
 
 export default About;
---
+++ src/scripts/updateFrontEnd.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+require('dotenv').config();
+const path = require('path');
+
+async function updateFrontEnd(deployedContractAddress) {
+  console.log('updating files');
+
+  console.log('updating contractAddress on FE...');
+  const data = `export const contractAddress = "${deployedContractAddress}";`;
+  const contractAddrPath = path.resolve(
+    __dirname,
+    '../utils/contractAddress.ts'
+  );
+
+  fs.writeFileSync(contractAddrPath, data, (err) => {
+    if (err) {
+      console.error('ERROR: contract address not updated');
+      throw err;
+    }
+  });
+  console.log(
+    `Successfully updated contract address to ${deployedContractAddress}`
+  );
+  return deployedContractAddress;
+}
+
+module.exports = { updateFrontEnd };
