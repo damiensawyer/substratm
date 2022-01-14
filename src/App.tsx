964d5a2b1312ba0246992b155a3b86e6d06c9c65
0xLil14n <0xLil14n@gmail.com>
2022-01-13 22:28:38 -0500

    Cleaning up app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 import NavigationBar from './components/navigation/navigationBar';
 import './App.css';
 import ScratchPad from './pages/scratchPad/scratchPad';
@@ -13,30 +14,38 @@ function App() {
   let isLoggedIn = useAppSelector((x) => x.loginSlice.loggedIn);
 
   return (
-    <div className="App">
-      <BrowserRouter>
-        <div>
-          <NavigationBar />
-          <Header />
-          <div className="App-header">
-            <Switch>
-              <Route exact path="/" render={() => <h1></h1>} />
-              {isLoggedIn && (
-                <>
-                  <Route path="/home" exact component={Home} />
-                  <Route path="/about" exact component={About} />
-                  {/*it would be good to render this only in dev mode if we can get that requireAuth function working */}
-                  <Route path="/scratchpad" exact component={ScratchPad} />
-                </>
-              )}
+    <BrowserRouter>
+      <StyledApp>
+        <NavigationBar />
+        <Header />
+        <StyledHeader>
+          <Switch>
+            <Route exact path="/" render={() => <h1></h1>} />
+            {isLoggedIn && (
+              <>
+                <Route path="/home" exact component={Home} />
+                <Route path="/about" exact component={About} />
+                {/*it would be good to render this only in dev mode if we can get that requireAuth function working */}
+                <Route path="/scratchpad" exact component={ScratchPad} />
+              </>
+            )}
 
-              {!isLoggedIn && <Route component={UnAuthenticatedView} />}
-            </Switch>
-          </div>
-        </div>
-      </BrowserRouter>
-    </div>
+            {!isLoggedIn && <Route component={UnAuthenticatedView} />}
+          </Switch>
+        </StyledHeader>
+      </StyledApp>
+    </BrowserRouter>
   );
 }
-
+const StyledApp = styled.div`
+  text-align: center;
+`;
+const StyledHeader = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+`;
 export default App;
